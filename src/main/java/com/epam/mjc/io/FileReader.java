accd9764e5ae2b0c3a99c231361b55ff33cc16f1
TERABYTE\inter <zhanibek.orazbay@pearson.com>
2023-10-21 18:36:18 +0600

    Refactored

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -16,21 +16,24 @@ public class FileReader {
                 char c = (char)ch;
 
                 if (key.toString().equals("Name") || key.toString().equals("Age") || key.toString().equals("Email") || key.toString().equals("Phone")) {
-                    if (c == '\r'){
-                        continue;
-                    } else if (c =='\n'){
-                        if (key.toString().equals("Name")){
-                            profile.setName(value.toString());
-                        } else if (key.toString().equals("Age")) {
-                            profile.setAge(Integer.parseInt(value.toString()));
-                        } else if (key.toString().equals("Email")){
-                            profile.setEmail(value.toString());
-                        } else {
-                            profile.setPhone(Long.parseLong(value.toString()));
+                    if (c =='\n'){
+                        switch (key.toString()) {
+                            case "Name":
+                                profile.setName(value.toString());
+                                break;
+                            case "Age":
+                                profile.setAge(Integer.parseInt(value.toString()));
+                                break;
+                            case "Email":
+                                profile.setEmail(value.toString());
+                                break;
+                            default:
+                                profile.setPhone(Long.parseLong(value.toString()));
+                                break;
                         }
                         key.delete(0,key.length());
                         value.delete(0,value.length());
-                    } else if (c != ':' && c != ' '){
+                    } else if (c != ':' && c != ' ' && c != '\r'){
                         value.append(c);
                     }
                 } else {
@@ -38,9 +41,9 @@ public class FileReader {
                 }
             }
         } catch (FileNotFoundException e) {
-
+            System.out.println("File not found");
         } catch (IOException e){
-
+            System.out.println("IO exception");
         }
         return profile;
     }
